bcbd203b48db7b8ead828333d7a678f2e1987299
DuyLuu90 <58050608+DuyLuu90@users.noreply.github.com>
2020-10-01 02:39:02 -0700

    update db

--- postgrator-config.js
+++ postgrator-config.js
@@ -5,6 +5,7 @@ module.exports = {
     "driver": "pg",
     "connectionString": (process.env.NODE_ENV === 'test')
         ? process.env.TEST_DATABASE_URL
-        : process.env.DATABASE_URL,
+        : process.env.DATABASE_URL ||
+        'postgres://ghpcgzjuxqymig:57b106eafbfb03708350e21605b21665b822779802fd8be911aca6abd39091b6@ec2-54-90-68-208.compute-1.amazonaws.com:5432/d41crvol1umhm',
     "ssl": !!process.env.SSL
 }
